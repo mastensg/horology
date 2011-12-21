75b4b355e9c3a8641aef5651a12fceba2c2611ef
Martin Stensgård <mastensg@ping.uio.no>
2011-12-21 18:13:54 +0100

    horology: remove end-of-line whitespace

--- horology.py
+++ horology.py
@@ -8,7 +8,7 @@ def gregorian_to_julian(year, month, day):
     a = (14 - month) // 12
     y = year + 4800 - a
     m = month + 12 * a - 3
-    
+
     return day + (153 * m + 2) // 5 + 365 * y + y // 4 - y // 100 + y // 400 - 32045
 
 def unix_to_julian(U):
