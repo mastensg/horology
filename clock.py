d292cc5c09b2a455fa6a2b6fbc96f8262d3e7e71
Martin Stensgård <mastensg@ping.uio.no>
2011-12-31 04:23:47 +0100

    clock: nicer code

--- clock.py
+++ clock.py
@@ -35,7 +35,7 @@ def draw_dial(dial):
     xo = w // 2
     yo = h // 2
 
-    r = 0.3 * min(sw, sh)
+    r = 0.3 * min(w, h)
     for i in range(1, 13):
         a = i / 12
         x, y = artoxy(a, r, xo, yo)
@@ -45,14 +45,14 @@ def draw_dial(dial):
 
         dial.blit(label, (x - lw // 2, y - lh // 2))
 
-    r = 0.4 * min(sw, sh)
+    r = 0.4 * min(w, h)
     for i in range(5, 61, 5):
         a = i / 60
         b = -360 * a
         x, y = artoxy(a, r, xo, yo)
 
         label = mf.render("%d" % i, True, fg)
-        label = pygame.transform.rotate(label, b)
+        label = pygame.transform.rotozoom(label, b, 1.0)
         lw, lh = label.get_size()
 
         dial.blit(label, (x - lw // 2, y - lh // 2))
@@ -60,12 +60,11 @@ def draw_dial(dial):
 def draw(screen, now):
     w, h = screen.get_size()
 
-    jdn = horology.unix_to_julian(now) + 1
-    sunrise, noon, sunset = horology.sun_events(jdn, lon, lat)
-    seconds = now % 60
-    minutes = (now / 60) % 60
-    hours = (now / 3600) % 12
-    #print int(hours), int(minutes), seconds
+    sunrise, noon, sunset = horology.sun_events(now, lon, lat)
+
+    local_now = now - time.timezone
+    minutes = (local_now / 60) % 60
+    hours = (local_now / 3600) % 12
 
     draw_dial(screen)
 
@@ -74,51 +73,42 @@ def draw(screen, now):
 
     xo = w // 2
     yo = h // 2
+    ro = 0.04 * min(w, h)
 
     a = hours / 12
-    r = 0.25 * min(sw, sh)
+    r = 0.22 * min(w, h)
     x, y = artoxy(a, r, xo, yo)
 
-    points = (x, y), artoxy(a - 0.25, 0.2 * r, xo, yo), artoxy(a + 0.25, 0.2 * r, xo, yo)
+    points = (x, y), artoxy(a - 0.25, ro, xo, yo), artoxy(a + 0.25, ro, xo, yo)
     gfxdraw.filled_polygon(screen, points, fg)
 
     a = minutes / 60
-    r = 0.38 * min(sw, sh)
+    r *= 1.61803
     x, y = artoxy(a, r, xo, yo)
 
-    points = (x, y), artoxy(a - 0.25, 0.1 * r, xo, yo), artoxy(a + 0.25, 0.1 * r, xo, yo)
+    points = (x, y), artoxy(a - 0.25, ro, xo, yo), artoxy(a + 0.25, ro, xo, yo)
     gfxdraw.filled_polygon(screen, points, fg)
 
-    a = seconds / 60
-    r = 0.38 * min(sw, sh)
-    x, y = artoxy(a, r, xo, yo)
-
-    gfxdraw.filled_circle(screen, xo, yo, int(0.05 * min(sw, sh)), fg)
+    gfxdraw.filled_circle(screen, xo, yo, int(ro), fg)
 
 def main():
     pygame.init()
-
     pygame.mouse.set_visible(False)
+    pygame.time.set_timer(pygame.VIDEOEXPOSE, 5000)
 
-    framerate = 1
-    pygame.time.set_timer(pygame.VIDEOEXPOSE, 1000 // framerate)
+    if "-f" in sys.argv:
+        w, h = pygame.display.list_modes()[0]
+        flags = pygame.FULLSCREEN
+    else:
+        w, h = 300, 300
+        flags = 0
 
-    global sw, sh
-    sw, sh = 800, 480
-    flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.FULLSCREEN
-    screen = pygame.display.set_mode((sw, sh), flags)
+    screen = pygame.display.set_mode((w, h), flags)
 
     global hf, mf
     hf = pygame.font.SysFont("Dejavu Sans", 56)
-    mf = pygame.font.SysFont("Dejavu Sans", 24)
+    mf = pygame.font.SysFont("Dejavu Sans", 34)
 
-    tz = 3600
-    while True:
-        now = time.time() + tz
-        draw(screen, now)
-        pygame.display.update()
-        time.sleep(3)
-    exit()
     while True:
         event = pygame.event.wait()
 
@@ -128,11 +118,13 @@ def main():
             pygame.display.update()
 
         elif event.type == pygame.VIDEORESIZE:
-            sw, sh = event.w, event.h
-            screen = pygame.display.set_mode((sw, sh), flags)
+            w, h = event.w, event.h
+            screen = pygame.display.set_mode((w, h), flags)
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                return 0
+            elif event.key == pygame.K_q:
                 return 0
 
         elif event.type == pygame.QUIT:
