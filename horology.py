325c8eaba7813c4deacd1b123530d50ec244806a
Martin Stensgård <mastensg@ping.uio.no>
2011-12-31 03:35:54 +0100

    horology: print sun events. add julian_to_unix

--- horology.py
+++ horology.py
@@ -1,7 +1,7 @@
 import math
 
 def gregorian_to_julian(year, month, day):
-    """Return the Julian Day Number from a Gregorian calendar date.
+    """Convert Gregorian date to Julian date.
 
     >>> gregorian_to_julian(2011, 12, 21)
     2455917
@@ -13,27 +13,11 @@ def gregorian_to_julian(year, month, day):
 
     return day + (153 * m + 2) // 5 + 365 * y + y // 4 - y // 100 + y // 400 - 32045
 
-def unix_to_julian(U):
-    """Return the Julian Day Number from seconds since the UNIX epoch.
-
-    >>> unix_to_julian(1324484390)
-    2455917
-    """
-
-    ss = U % 60
-    a = (U - ss) // 60
-    mm = a % 60
-    b = (a - mm) // 60
-    hh = b % 24
-    u = U - ss - mm * 60 - hh * 3600
-
-    return u // 86400 + 2440588
-
 def julian_to_gregorian(JD):
-    """Return the Gregorian calendar date from a Julian Day Number.
+    """Convert Julian date to Gregorian date.
 
-    >>> julian_to_gregorian(2455916.969553709)
-    (2011, 12, 21.46955370903015)
+    >>> julian_to_gregorian(2455916.5)
+    (2011, 12, 21.0)
     """
 
     J = JD + 0.5
@@ -61,25 +45,29 @@ def julian_to_gregorian(JD):
 
     return Y, M, D
 
-def days_to_hms(days):
-    """Return hours, minutes and seconds from days.
-
-    >>> days_to_hms(0.46955370903)
-    (12, 16, 9.440460192000549)
+def julian_to_unix(jd):
+    """Convert Julian Date to Unix time.
     """
 
-    h = int(days * 24)
-    m = int((days - h / 24.) * 24 * 60)
-    s = (days - h / 24. - m / (24. * 60.)) * 24 * 60 * 60
+    return (jd - 2440587.5) * 86400
 
-    return h + 1, m, s
+def unix_to_julian(u):
+    """Convert Unix time to Julian Date.
 
-def sun_events(J_date, l_w, phi):
-    """Return the Julian day times for sunrise, solar transit, and sunset.
-
-    >>> sun_events(2455917, -10.756389, 59.949444)
-    (0.32285522762686014, 0.46955370903015137, 0.6162521904334426)
+    >>> int(unix_to_julian(1324484390))
+    2455917
     """
+
+    return u / 86400. + 2440587.5
+
+def sun_events(t, l_w, phi):
+    """Return the times for sunrise, solar transit, and sunset.
+
+    >>> map(int, sun_events(2455917, -10.756389, 59.949444))
+    [2445911, 2460681, 2475451]
+    """
+
+    J_date = unix_to_julian(t)
 
     acos = lambda x: math.degrees(math.acos(x))
     asin = lambda x: math.degrees(math.asin(x))
@@ -116,17 +104,23 @@ def sun_events(J_date, l_w, phi):
     J_set = 2451545 + 0.0009 + ((omega_0 + l_w) / 360. + n + 0.0053 * sin(M)) - 0.0069 * sin(2 * Lambda)
     J_rise = J_transit - (J_set - J_transit)
 
-    return tuple([j - J_date + 0.5 for j in J_rise, J_transit, J_set])
+    return tuple(map(julian_to_unix, [J_rise, J_transit, J_set]))
 
 if __name__ == "__main__":
     import doctest
     failed, attempted = doctest.testmod()
-    if failed == 0:
+    if False and failed == 0:
         print "Yay! All %d tests passed!" % attempted
 
+    import time
     lon = -10.756389
     lat = 59.949444
-    for i in range(7):
-        jd = 2455917 + i
-        up, noon, down = ["%02d:%02d:%02d" % days_to_hms(j) for j in sun_events(jd, lon, lat)]
-        print "%d\t%s\t%s\t%s" % (i, up, noon, down)
+
+    now = time.time()
+
+    format = lambda t: time.strftime("%Y-%m-%d %H:%M:%S %Z", time.localtime(t))
+
+    up, noon, down = sun_events(now, lon, lat)
+    print "Sunrise:         %s" % format(up)
+    print "Solar transit:   %s" % format(noon)
+    print "Sunset:          %s" % format(down)
